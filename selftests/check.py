083b44ace6ad0a395d90db9114e756ece1b89f71
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2026-08-17 19:47:55 +0800

    selftests: account for resilience coverage

--- selftests/check.py
+++ selftests/check.py
@@ -127,7 +127,7 @@ TEST_SIZE = {
     "job-api-check-tmp-directory-exists": 1,
     "nrunner-interface": 90,
     "nrunner-requirement": 28,
-    "unit": 1028,
+    "unit": 1044,
     "jobs": 11,
     "functional-parallel": 374,
     "functional-serial": 7,
